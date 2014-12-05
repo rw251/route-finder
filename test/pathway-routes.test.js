e915a30180780d1a8ca2919bc23f441794fe564c
Richard <richard.williams2@man.ac.uk>
2014-12-05 09:30:22 +0000

    Added passing tests

---
+++ test/pathway-routes.test.js
@@ -0,0 +1,14 @@
+var expect = require("chai").expect;
+var routes = require("../lib/pathway-routes.js");
+
+describe("Routes", function() {
+    describe("#getRoutes()", function(){
+        it("should return an empty array for an empty pathway", function(){
+            var pathway = {};
+            var results = routes.getRoutes(pathway);
+                                    
+            expect(results).to.be.instanceof(Array);
+            expect(results).to.be.empty();
+        });
+    });
+});
