eeeff0a318fc11a2a8678c8da551e77618c5968d
Richard <richard.williams2@man.ac.uk>
2014-12-05 14:24:33 +0000

    Minor mod

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var fs = require('fs')
-var dot = require('graphlib-dot')
+var fs = require('fs');
+var dot = require('graphlib-dot');
 
 var digraph = dot.read(fs.readFileSync('examples/stroke.dot', 'UTF-8'));
 
