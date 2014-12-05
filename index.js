13925c26f28b9bd7578ad56c0b96e75af8a014a9
Richard <richard.williams2@man.ac.uk>
2014-12-05 09:01:17 +0000

    Example stroke pathway added

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log("hello world");
-5+5;
+"use strict";
+
+var fs = require('fs')
+var dot = require('graphlib-dot')
+
+var digraph = dot.read(fs.readFileSync('examples/stroke.dot', 'UTF-8'));
+
+console.log(digraph.nodes());
+console.log(digraph.edges());
