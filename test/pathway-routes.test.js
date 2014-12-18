1b9bca759d713751ab1bf3faaad6174f2bc983ba
Richard <richard.williams2@man.ac.uk>
2014-12-18 12:24:16 +0000

    added natural package

--- test/pathway-routes.test.js
+++ test/pathway-routes.test.js
@@ -1,14 +1,37 @@
+"use strict";
+
 var expect = require("chai").expect;
 var routes = require("../lib/pathway-routes.js");
+var describe = require("mocha").describe;
+var it = require("mocha").it;
+var fs = require('fs');
+var dot = require('graphlib-dot');
 
 describe("Routes", function() {
-    describe("#getRoutes()", function(){
-        it("should return an empty array for an empty pathway", function(){
+    describe(".getRoutes()", function(){
+        it("should return an empty array for an empty pathway", function(done){
             var pathway = {};
-            var results = routes.getRoutes(pathway);
-                                    
-            expect(results).to.be.instanceof(Array);
-            expect(results).to.be.empty();
+            routes.getRoutes(pathway, function(err, data){
+                if(err) return done(err);
+
+                expect(data).to.be.instanceof(Array);
+                expect(data).to.be.empty();
+
+                done();
+            });
+        });
+    });
+    describe(".getRouteStrings()", function(){
+        it("should return an empty array for an empty pathway", function(done){
+            var graph = { nodes: function(){return [];}, edges: function(){return [];} };
+            routes.getRouteStrings(graph, null, function(err, data){
+                if(err) return done(err);
+
+                expect(data).to.be.instanceof(Array);
+                expect(data).to.be.empty();
+
+                done();
+            });
         });
     });
 });
